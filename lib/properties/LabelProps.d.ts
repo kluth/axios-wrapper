323f60c5576e54eb79ff74ee9e455eb85fe657e4
Matthias Kluth <km@paibosoft.de>
2022-10-20 19:22:09 +0200

    fix(repo): cleanup

---
+++ lib/properties/LabelProps.d.ts
@@ -0,0 +1,4 @@
+export interface LabelProps {
+    name: 'bug' | 'documentation' | 'duplicate' | 'enhancement' | 'good first issue' | 'help wanted' | 'invalid' | 'question' | 'wontfix';
+    color: string;
+}
